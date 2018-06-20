4239d70eb11760a979b13a475faee21b6b730cd7
sahitikappagantula <sahiti.kappagantula@edureka.co>
2018-06-20 15:27:45 +0530

    All files in the folder are commited

--- Demo.java
+++ Demo.java
@@ -1,1 +1,8 @@
-Hi there! I'm the java file
+class Demo{
+public static void main (String[] args)
+{
+System.out.println("Hi there");
+System.out.println("This is a Demo file");
+System.out.println("Welcome to git");
+}
+
